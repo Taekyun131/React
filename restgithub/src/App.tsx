8e800174a6ee0b665fe19985f062ebd83de44cf6
Taekyun <stk131@naver.com>
2026-02-13 23:09:00 +0900

    20260213 리액트로 REST API 이용

---
+++ restgithub/src/App.tsx
@@ -0,0 +1,50 @@
+import { useState } from 'react'
+import './App.css'
+import axios from 'axios'
+
+type Repository = {
+  id: number;
+  full_name: string;
+  html_url: string;
+};
+
+
+function App() {
+  const [keyword, setKeyword] = useState('');
+  const [repodata, setRepodata] = useState<Repository[]>([]);
+
+  const handleClick = () => {
+    // REST API 호출
+    axios.get<{items: Repository[] }> (`https://api.github.com/search/repositories?q=${keyword}`)
+    .then(response => setRepodata(response.data.items))
+    .catch(err => console.error(err))
+  }
+
+  return (
+    <>
+      <input
+        value={keyword}
+        onChange={e => setKeyword(e.target.value)}
+      />
+      <button onClick={handleClick}>fetch</button>
+      {repodata.length === 0 ? (
+        <p>No data available</p>
+      ):(
+        <table>
+          <tbody>
+            {repodata.map(repo => (
+              <tr key={repo.id}>
+                <td>{repo.full_name}</td>
+                <td>
+                  <a href={repo.html_url}>{repo.html_url}</a>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
+    </>
+  )
+}
+
+export default App
---
+++ weatherapp/src/App.jsx
@@ -0,0 +1,43 @@
+import { useState, useEffect } from 'react'
+import './App.css'
+
+
+function App() {
+  const APIKey = import.meta.env.VITE_WEATHER_API_KEY;
+  const [weather, setWeather] = useState({
+    temp: '',
+    desc: '', 
+    icon: ''
+  });
+
+  useEffect(() => {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=London&APIKey=${APIKey}&units=metric`)
+    .then(response => response.json())
+    .then(result => {
+      setWeather({
+        temp: result.main.temp,
+        desc: result.weather[0].main,
+        icon: result.weather[0].icon,
+      });
+    })
+    .catch(err => console.error(err))
+  }, [])
+
+  
+  if(weather.icon){
+    return (
+      <>
+        <p>Temperature: {weather.temp}</p>
+        <p>Description: {weather.desc}</p>
+        <img 
+          src={`http://openweathermap.org/img/wn/${weather.icon}@2x.png`} 
+          alt="Weather icon"
+        />  
+      </>
+    )
+  } else {
+    return <div>Loading...</div>
+  }
+}
+
+export default App
